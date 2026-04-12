484bded6c8536233c2c979b4249c64652dbeb4c0
Inuzuka <inuzuka8080@gmail.com>
2026-04-13 08:47:48 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2082,21 +2082,29 @@ const handleTextChange = (textId) => {
         {/* アプリ概要（フランス語 / Cinzel） */}
   　<div className="mb-10 text-center select-none">
     <h2 
-      style={{ fontFamily: '"Cinzel", serif', letterSpacing: '0.15em' }}
+      style={{ fontFamily: '"Cinzel", serif', letterSpacing: '0.2em' }}
       className={`text-[10px] mb-3 ${textSecondary}`}
     >
       Vanitas vanitatum et omnia vanitas.<br />
-      Quare vanitatem repraesenta.
+      Quare vanitatem repræsenta.
     </h2>
     <p 
       style={{ fontFamily: '"Cinzel", serif', letterSpacing: '0.13em' }}
       className={`text-[9px] max-w-xs mx-auto ${textSecondary}`}
     >
-      VANITISME; L'archive d’ombres et de silence. Ce dispositif met à nu le désespoir d’un langage qui ne rencontre que son propre vide.
-      Pourtant, si le verbe est impuissant à saisir l’être, cela ne justifie en rien l’arrêt de cet acte de résistance, bien que vain. 
-      Contre le nihilisme de l'oubli, nous dressons la splendeur de l'éphémère.
-      Si tout est vide, alors rien n'interdit de sculpter, au sein du néant, la lumière, l'ombre, le parfum et le rythme.
-      Car représenter la vanité, c'est encore, et toujours, créer et renouveler un monde.
+      VANITISME; L'archive d’ombres et de silence. <br />
+      Ce dispositif met à nu le désespoir d’un langage<br />
+      qui ne rencontre que son propre vide.<br />
+      Pourtant, si le verbe est impuissant à saisir l’être, 
+      cela ne justifie en rien l’arrêt de cet acte de Résistance, 
+      bien que vain. <br />
+      Contre le nihilisme de l'oubli,<br />
+      nous dressons la splendeur de l'éphémère.<br />
+      Si tout est vide, alors rien n'interdit<br />
+      de sculpter, au sein du néant,<br />
+      la lumière, l'ombre, le parfum et le rythme.<br />
+      Car représenter la vanité,<br />
+      c'est encore, et toujours, créer et renouveler un monde.<br />
     </p>
   </div>
         <div className={`text-center text-[9px] font-IBM Plex sans JP ${textSecondary} pb-8 space-y-1`}>
