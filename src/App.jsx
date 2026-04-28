e2f510996a83f1dc918047fd5939941625d1a6f6
Inuzuka <inuzuka8080@gmail.com>
2026-04-28 18:41:09 +0900

    Change text size from 11px to small in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2389,7 +2389,7 @@ const TocDrawer = ({
                             </div>
                           );
                         })}
-                        <div className={`text-center py-1.5 ${tocOrnament} text-[11px] tracking-[0.4em] select-none`}>
+                        <div className={`text-center py-1.5 ${tocOrnament} text-sm tracking-[0.4em] select-none`}>
                           ⚜　⚜　⚜
                         </div>
                       </div>
