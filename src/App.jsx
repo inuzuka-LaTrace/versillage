abb32402abbed7c734773a27bc75b553cff5dcf4
Inuzuka <inuzuka8080@gmail.com>
2026-04-29 09:30:45 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2307,7 +2307,7 @@ const TocDrawer = ({
               <button
                 key={key}
                 onClick={() => setTocLangFilter(key)}
-                className={`px-2 py-2 text-sm font-EB Garamond rouded-xs transition-colors border ${
+                className={`px-2 py-2 text-sm font-EB Garamond rouded-sm transition-colors border ${
                   tocLangFilter === key ? tocChipAct : tocChipInact
                 }`}
               >
